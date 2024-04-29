234c8e49a6e57b90fe01ace5d9612f8ed2deb46f
Pablo Lopez <plopesc@gmail.com>
2024-04-29 16:18:56 +0200

    Create PhpCsCommand.php

---
+++ core-dev/src/Command/PhpCsCommand.php
@@ -0,0 +1,36 @@
+<?php
+#ddev-generated
+
+namespace DrupalCoreDev\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class PhpCsCommand extends Command {
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void {
+        $this->setName('phpcs')
+          ->setDescription('Run PHPCS code analysis against core.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int {
+        $command = "composer phpcs -- --report-full";
+        $phpcs = Process::fromShellCommandline($command);
+        $output->writeln($command);
+        $phpcs->setTimeout(0);
+        $phpcs->run(function ($type, $data) use ($output) {
+            $output->write($data);
+        });
+        if ($phpcs->getExitCode()) {
+            return 1;
+        }
+        return 0;
+    }
+}
